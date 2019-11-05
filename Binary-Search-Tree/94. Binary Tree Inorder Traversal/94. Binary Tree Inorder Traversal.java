a9a9f4371301bd68b5f186d29b4751ce0564359d
Naisheng Zhang <jonathan.n.zhang@gmail.com>
2019-11-05 18:05:46 -0500

    94

---
+++ Binary-Search-Tree/94. Binary Tree Inorder Traversal/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        List<Integer> ans = new ArrayList();
+        if (root == null) {
+            return ans;
+        }
+
+        while (root != null || !stack.isEmpty()) {      
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            ans.add(root.val);
+            root = root.right;
+        }
+        return ans;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # 目录
 
-## Stack
+
+
+## [Stack](https://github.com/NaishengZhang/leetcode/tree/master/Stack)
 
 ### [173. Binary Search Tree Iterator]()
 
